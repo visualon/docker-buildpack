bcde0b697a2df48f4400ea84b020d719211e0920
Michael Kriese <michael.kriese@visualon.de>
2021-11-10 10:55:17 +0100

    fix(vs-tools): use new components

--- windows/src/tools/installer/vs-tools.ps1
+++ windows/src/tools/installer/vs-tools.ps1
@@ -5,15 +5,19 @@ Invoke-WebRequest -OutFile c:\TEMP\vs_BuildTools.exe https://aka.ms/vs/17/releas
 
 #curl -sSfLo c:\TEMP\collect.exe https://aka.ms/vscollect.exe
 
-Write-Debug "Installing vs build tools"
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2022
+Write-Debug "Installing vs 2022 build tools"
 exec {
   c:\TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache `
     --installPath C:\BuildTools  `
-    --add Microsoft.VisualStudio.Workload.WebBuildTools  `
-    --add Microsoft.VisualStudio.Workload.OfficeBuildTools  `
-    --add Microsoft.VisualStudio.Workload.NetCoreBuildTools `
-    --add Microsoft.NetCore.Component.Runtime.5.0 | Out-Null
-}  -AllowedExitCodes  @(0, 3010)
+		--add Microsoft.VisualStudio.Workload.MSBuildTools `
+		--add Microsoft.VisualStudio.Workload.WebBuildTools  `
+		--add Microsoft.VisualStudio.Workload.OfficeBuildTools  `
+    --add Microsoft.NetCore.Component.Runtime.3.1 `
+		--add Microsoft.NetCore.Component.Runtime.5.0 `
+		--add Microsoft.NetCore.Component.Runtime.6.0 `
+		--add Microsoft.NetCore.Component.SDK
+} -AllowedExitCodes  @(0, 3010)
 
 Write-Debug "Installing vs done"
 
@@ -28,6 +32,7 @@ if ($err = Get-ChildItem $Env:TEMP -Filter dd_setup_*_errors.log | Where-Object 
 
 install-shim msbuild C:\BuildTools\MSBuild\Current\Bin\amd64\msbuild.exe
 install-shim dotnet ${env:ProgramFiles}\dotnet\dotnet.exe
+install-shim vswhere "${env:ProgramFiles(X86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 
 Write-Debug "VS Test ..."
 dotnet nuget list source
