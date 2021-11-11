d415a88409ec6fed3ab33f52b0145a813d5ffabe
Michael Kriese <michael.kriese@visualon.de>
2021-11-11 08:54:47 +0100

    add semver release

---
+++ bin/prepare-release.js
@@ -0,0 +1,17 @@
+import shell from 'shelljs';
+import { opts } from '../util/index.js';
+
+const version = opts.release;
+const image = process.env.IMAGE;
+
+shell.echo(`Preparing version: ${version}`);
+
+let r = shell.exec(`docker manifest create ${image}:${version} ${image}:linux ${image}:windows-2019 ${image}:windows-2022`);
+if (r.code) {
+  shell.exit(1);
+}
+
+r = shell.exec(`docker manifest inspect ${image}:${version}`);
+if (r.code) {
+  shell.exit(1);
+}
