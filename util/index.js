ada517dd68c70048e3641ff646fa737a8e4b099f
Michael Kriese <michael.kriese@visualon.de>
2022-02-09 12:03:44 +0100

    chore: fix migration

--- util/index.js
+++ util/index.js
@@ -1,6 +1,6 @@
-import commander from 'commander';
+import { Command } from 'commander';
 
-const program = new commander.Command();
+const program = new Command();
 program.version('0.0.1').requiredOption('-r, --release <type>', 'Version to use').option('-s, --sha <type>', 'Sha to use').option('-d, --dry-run');
 
 program.parse(process.argv);
