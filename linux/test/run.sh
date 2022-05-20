abf1b2bc68db109a962e622bf5fd76be89f6d9ad
Michael Kriese <michael.kriese@visualon.de>
2022-05-20 14:28:02 +0200

    feat: add new skopeo prebuild

--- linux/test/run.sh
+++ linux/test/run.sh
@@ -6,3 +6,6 @@ git --version
 
 # renovate: datasource=github-releases lookupName=minio/mc
 install-tool minio-client RELEASE.2022-05-09T04-08-26Z
+
+# renovate: datasource=github-releases lookupName=visualon/skopeo-prebuild
+RUN install-tool skopeo v1.8.0
