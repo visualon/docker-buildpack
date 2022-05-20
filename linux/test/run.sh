9c3f0acb6190619cb39b6388159252828f4acc25
Michael Kriese <michael.kriese@visualon.de>
2022-05-20 15:18:27 +0200

    chore: fix tests

--- linux/test/run.sh
+++ linux/test/run.sh
@@ -8,4 +8,6 @@ git --version
 install-tool minio-client RELEASE.2022-05-09T04-08-26Z
 
 # renovate: datasource=github-releases lookupName=visualon/skopeo-prebuild
-RUN install-tool skopeo v1.8.0
+install-tool skopeo v1.8.0
+
+skopeo inspect docker://registry.fedoraproject.org/fedora:latest | jq -r '.Name+"@"+.Digest'
