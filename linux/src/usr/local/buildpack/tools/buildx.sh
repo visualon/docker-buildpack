33174e5277c6830b3f3ba4e0d51818636ddb0240
Michael Kriese <michael.kriese@visualon.de>
2022-05-20 12:40:25 +0200

    fix: support docker compose v2

--- linux/src/usr/local/buildpack/tools/buildx.sh
+++ linux/src/usr/local/buildpack/tools/buildx.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-check_semver ${TOOL_VERSION}
+check_semver "${TOOL_VERSION}"
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${TOOL_VERSION}
+  echo Invalid version: "${TOOL_VERSION}"
   exit 1
 fi
 
@@ -13,9 +13,9 @@ DISTRO=linux-amd64
 URL=https://github.com/docker/buildx/releases/download/v${TOOL_VERSION}/buildx-v${TOOL_VERSION}.${DISTRO}
 TARGET=${HOME}/.docker/cli-plugins/docker-buildx
 
-mkdir -p ${HOME}/.docker/cli-plugins
+mkdir -p "${HOME}"/.docker/cli-plugins
 
-curl -sL $URL -o ${TARGET}
-chmod +x ${TARGET}
+curl -sL "$URL" -o "${TARGET}"
+chmod +x "${TARGET}"
 
 docker buildx version
