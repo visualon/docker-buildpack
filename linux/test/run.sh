4faaa759414aebeddb0900ffaad254867077fce6
Michael Kriese <michael.kriese@visualon.de>
2022-05-25 11:11:03 +0200

    test: install docker for tests

--- linux/test/run.sh
+++ linux/test/run.sh
@@ -15,11 +15,14 @@ skopeo inspect docker://registry.fedoraproject.org/fedora:latest | jq -r '.Name+
 # renovate: datasource=github-tags lookupName=kubernetes/kubectl
 install-tool kubectl 1.20.1
 
+# renovate: datasource=github-releases lookupName=moby/moby
+install-tool docker v20.10.16
+
+# renovate: datasource=github-releases lookupName=docker/compose
+install-tool docker-compose 1.29.2
+
 # renovate: datasource=github-releases lookupName=docker/buildx
 install-tool buildx v0.8.2
 
 # renovate: datasource=github-releases lookupName=kubernetes-sigs/kustomize
 install-tool kustomize 4.0.4
-
-# renovate: datasource=github-releases lookupName=docker/compose
-install-tool docker-compose 1.29.2
