a0c13272dd1cba437df8345f8a4588e568e51217
Michael Kriese <michael.kriese@visualon.de>
2023-01-17 14:57:30 +0100

    feat: add dotnet runtime installer

--- windows/src/tools/lib/index.ps1
+++ windows/src/tools/lib/index.ps1
@@ -5,3 +5,18 @@ $lib = $PSScriptRoot
 . $lib\shim.ps1
 . $lib\exec.ps1
 . $lib\cache.ps1
+
+
+function set-dotnet-env {
+  if($env:DOTNET_ROOT){
+    Write-Debug "Env already configured"
+    return;
+  }
+  Write-Debug "Configure env ..."
+  [Environment]::SetEnvironmentVariable("DOTNET_ROOT", "${env:ProgramFiles}\dotnet", "Machine")
+  [Environment]::SetEnvironmentVariable("DOTNET_NOLOGO", "true", "Machine")
+  [Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "true", "Machine")
+  [Environment]::SetEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-us", "Machine")
+  [Environment]::SetEnvironmentVariable("PATH", "${env:ProgramFiles}\dotnet;$([Environment]::GetEnvironmentVariable("PATH", "Machine"))", "Machine")
+  Write-Debug "Configure env done"
+}
