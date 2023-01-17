b7a0e9c49b98567166d436380f5b360f2ee63790
Michael Kriese <michael.kriese@visualon.de>
2023-01-17 14:57:05 +0100

    fix: support newer docfx

--- windows/src/tools/installer/docfx.ps1
+++ windows/src/tools/installer/docfx.ps1
@@ -1,16 +1,21 @@
 #Requires -Version 5.1
 
-if ( -not ($Version -match '^(\d+\.\d+\.\d+)$') ) {
+if ( -not ($Version -match '^(\d+)\.(\d+)\.\d+$') ) {
   throw "Invalid $Name version"
 }
 
-
-$file = "$tmp\$Name.zip"
+$major = $Matches.1
+$minor = $Matches.2
 $app = "$apps\$Name"
-$url = "https://github.com/dotnet/docfx/releases/download/v$Version/docfx.zip"
 
-Invoke-WebRequest $url -OutFile $file
+if ($major -gt 2 -or ($major -eq 2 -and $minor -ge 60)){
+  dotnet tool install docfx --tool-path $app --version $Version
+} else {
+  $file = "$tmp\$Name.zip"
+  $url = "https://github.com/dotnet/docfx/releases/download/v$Version/docfx.zip"
 
-Expand-Archive -Path $file -DestinationPath $app
+  Invoke-WebRequest $url -OutFile $file
+  Expand-Archive -Path $file -DestinationPath $app
+}
 
 Install-Shim -Name docfx -Path docfx.exe
