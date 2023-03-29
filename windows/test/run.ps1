41e100625796cbfbf0b70fe364ca1932ddcae411
Michael Kriese <michael.kriese@visualon.de>
2023-03-29 10:53:49 +0200

    test: update tests

--- windows/test/run.ps1
+++ windows/test/run.ps1
@@ -5,27 +5,28 @@ $DebugPreference = 'Continue'
 
 . c:\tools\lib\exec.ps1
 
-pwsh --version
-ThrowOnNativeFailure
-
 # renovate: datasource=github-releases packageName=PowerShell/PowerShell
 install-tool powershell v7.3.3
-exec { pwsh --version }
+pwsh --version2
+ThrowOnNativeFailure
 
 # renovate: datasource=adoptium-java packageName=java-jre
 install-tool java 11.0.18+10
-exec { java --version }
+java --version
+ThrowOnNativeFailure
 
 # renovate: datasource=node
 install-tool node 18.15.0
-exec { node --version }
+node --version
+ThrowOnNativeFailure
 
 # renovate: datasource=dotnet packageName=dotnet-runtime
 install-tool dotnet 6.0.15
 
 # renovate: datasource=github-releases packageName=dotnet/docfx
 install-tool docfx v2.59.4
-exec { docfx --version }
+docfx --version
+ThrowOnNativeFailure
 
 # renovate: datasource=github-releases packageName=facebook/zstd
 install-tool zstd v1.5.4
@@ -36,4 +37,5 @@ install-tool minio-client RELEASE.2023-03-23T20-03-04Z
 # install last to get latest vs updates
 install-tool vs-tools
 
-exec { dotnet build test-cli }
+dotnet build test-cli
+ThrowOnNativeFailure
