184263686dcf11b2073acebc1a1fb508bdeba461
Michael Kriese <michael.kriese@visualon.de>
2023-03-30 06:44:38 +0200

    feat(windows): add vs redist v17
    Needed to load node natives which depend on `vcruntime`.

---
+++ windows/src/tools/installer/vc-redist.ps1
@@ -0,0 +1,21 @@
+#Requires -Version 5.1
+
+if ( -not ($Version -match '^(\d+)$') ) {
+  throw "Invalid $Name version"
+}
+
+Write-Debug "Downloading installer ..."
+Invoke-WebRequest -OutFile "$tmp\vc_redist.exe" https://aka.ms/vs/$Version/release/vc_redist.x64.exe
+Write-Debug "Downloading installer done"
+
+Write-Debug "Installing vc redist"
+$vsArgs = @(
+  "/install", "/quiet", "/norestart"
+)
+$vsProcess = Start-Process "$tmp\vc_redist.exe" -ArgumentList $vsArgs -Wait -PassThru -NoNewWindow
+
+if ($vsProcess.ExitCode -notin @(0, 3010)) {
+  throw "Install failed."
+}
+
+Write-Debug "Installing vc redist done"
