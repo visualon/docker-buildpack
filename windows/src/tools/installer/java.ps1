f3bf01f6cb27f50f2ecdc315866a5dfac3606469
Michael Kriese <michael.kriese@visualon.de>
2023-09-11 15:58:24 +0200

    fix(java): patch version

--- windows/src/tools/installer/java.ps1
+++ windows/src/tools/installer/java.ps1
@@ -3,8 +3,16 @@
 $ErrorActionPreference = 'Stop'
 
 # 11.0.12+7
-if ( -not ($Version -match '^(\d+\.\d+\.\d+(\+\d+)?)$') ) {
+if ( -not ($Version -match '^(?<version>\d+\.\d+\.\d+)(\+(?<meta>\d+))?$') ) {
   throw "Invalid $Name version"
+}
+
+$ver = $Matches.version;
+
+# patch 11.0.20+101 to 11.0.20.1+1
+if ($Matches.meta -match '^(?<patch>\d)(?<build>\d\d)$') {
+  $Version = $ver
+  $Version += '.' + $Matches.patch + '+' + (0 + $Matches.build)
 }
 
 $api = 'https://api.adoptium.net/v3/assets/version'
@@ -20,7 +28,7 @@ $url = $res[0].binaries[0].package.link
 
 Invoke-WebRequest $url -OutFile $file
 
-New-Item -Path $app -ItemType "directory" | Out-Null
+New-Item -Path $app -ItemType 'directory' | Out-Null
 
 tar -xzf $file -C $app --strip-components=1
 ExitOnNativeFailure
