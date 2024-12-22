348c3d7a5b4dabdc82a3ab8b9036a542984e76bb
Michael Kriese <michael.kriese@visualon.de>
2024-12-22 14:36:21 +0100

    feat: add windows 2025

--- bin/prepare-release.js
+++ bin/prepare-release.js
@@ -6,7 +6,7 @@ const image = process.env.IMAGE;
 
 shell.echo(`Preparing version: ${version}`);
 
-let r = shell.exec(`docker manifest create ${image}:${version} ${image}:linux ${image}:windows-2019 ${image}:windows-2022`);
+let r = shell.exec(`docker manifest create ${image}:${version} ${image}:linux ${image}:windows-2019 ${image}:windows-2022 ${image}:windows-2025`);
 if (r.code) {
   shell.exit(1);
 }
