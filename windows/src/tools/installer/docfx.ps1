afde89fbdb3046acc1be921721132c4e4974b1b9
Michael Kriese <michael.kriese@visualon.de>
2025-01-08 10:52:46 +0100

    feat: support newer docfx

--- windows/src/tools/installer/docfx.ps1
+++ windows/src/tools/installer/docfx.ps1
@@ -7,15 +7,15 @@ if ( -not ($Version -match '^(\d+)\.(\d+)\.\d+$') ) {
 $major = $Matches.1
 $minor = $Matches.2
 $app = "$apps\$Name"
+$file = "$tmp\$Name.zip"
 
 if ($major -gt 2 -or ($major -eq 2 -and $minor -ge 60)){
-  dotnet tool install docfx --tool-path $app --version $Version
+  $url = "https://github.com/dotnet/docfx/releases/download/v${Version}/docfx-win-x64-${Version}.zip"
 } else {
-  $file = "$tmp\$Name.zip"
   $url = "https://github.com/dotnet/docfx/releases/download/v$Version/docfx.zip"
-
-  Invoke-WebRequest $url -OutFile $file
-  Expand-Archive -Path $file -DestinationPath $app
 }
+
+Invoke-WebRequest $url -OutFile $file
+Expand-Archive -Path $file -DestinationPath $app
 
 Install-Shim -Name docfx -Path docfx.exe
