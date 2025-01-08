b5fdad5c0ac2e2f597628961323b47ecf99fd792
Michael Kriese <michael.kriese@visualon.de>
2025-01-08 15:46:32 +0100

    fix(docfx): support v2.78.1+

--- windows/src/tools/installer/docfx.ps1
+++ windows/src/tools/installer/docfx.ps1
@@ -1,17 +1,21 @@
 #Requires -Version 5.1
 
-if ( -not ($Version -match '^(\d+)\.(\d+)\.\d+$') ) {
+if ( -not ($Version -match '^(\d+)\.(\d+)\.(\d+)$') ) {
   throw "Invalid $Name version"
 }
 
 $file = "$tmp\$Name.zip"
 $major = $Matches.1
 $minor = $Matches.2
+$patch = $Matches.3
 $app = "$apps\$Name"
-
-if ($major -gt 2 -or ($major -eq 2 -and $minor -ge 60)){
+if ($major -gt 2 -or ($major -eq 2 -and $minor -gt 78) -or ($major -eq 2 -and $minor -eq 78 -and $patch -ge 1)) {
+  $url = "https://github.com/dotnet/docfx/releases/download/v${Version}/docfx-win-x64-${Version}.zip"
+}
+elseif ($major -eq 2 -and $minor -ge 60) {
   $url = "https://github.com/dotnet/docfx/releases/download/v${Version}/docfx-win-x64-v${Version}.zip"
-} else {
+}
+else {
   $url = "https://github.com/dotnet/docfx/releases/download/v$Version/docfx.zip"
 }
 
